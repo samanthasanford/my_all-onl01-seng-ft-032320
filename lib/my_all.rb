85fe37849a1b524ab1ab2f6b95e8fae7636bf826
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-26 05:00:01 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -22,3 +22,28 @@ def my_all?([1,2,3])
   i = 0 + 1 
 end
 end
+
+def my_all?(collection)
+  i = 0 
+  block_return_values = []
+  while i < collection.length 
+  block_return_values << yield(collection[i])
+  i += 1 
+end
+end
+
+def my_all?(collection)
+  i = 0 
+  block_return_values = []
+  while i < collection.length 
+  block_return_values << yield(collection[i])
+  i += 1 
+end
+
+if block_return_values.include?(false)
+  false
+else
+  true 
+  
+end
+end
