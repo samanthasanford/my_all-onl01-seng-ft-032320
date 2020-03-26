96331deb4c5616ee5a8c541b8f1e39fa4644f433
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-26 05:06:47 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,15 +2,15 @@ require pry
 
 def my_all?(collection)
   i = 0 
-  block_return_values = []
+  block_return_value = []
   
   while i < collection.length 
   
-  block_return_values << yield(collection[i])
+  block_return_value << yield(collection[i])
   i += 1 
 end
 
-if block_return_values.include?(false)
+if block_return_value.include?(false)
   false 
 else 
   true 
