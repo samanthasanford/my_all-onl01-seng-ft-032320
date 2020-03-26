c0285b8a80be03a03a4a710f56b18e0fe8aecf59
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-26 05:04:14 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,36 +1,4 @@
-require 'pry'
-
-def my_all?(collection)
-  i = 0 
-  while i < collection.length 
-  i += 1 
-end 
-end
-
-def my_all?(collection)
-  i = 0 
-  while i < collection.length 
-  yield(collection[i])
-  i += 1 
-end 
-end 
-
-def my_all?([1,2,3])
-  i = 0 
-  while i < 3 
-  yield(1)
-  i = 0 + 1 
-end
-end
-
-def my_all?(collection)
-  i = 0 
-  block_return_values = []
-  while i < collection.length 
-  block_return_values << yield(collection[i])
-  i += 1 
-end
-end
+require pry 
 
 def my_all?(collection)
   i = 0 
@@ -41,9 +9,12 @@ def my_all?(collection)
 end
 
 if block_return_values.include?(false)
-  false
-else
+  false 
+else 
   true 
-  
 end
-end
+end 
+
+my_all?([1,2,3]) do |i| i < 2 
+
+end 
